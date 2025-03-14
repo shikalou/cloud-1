9f54c197e91e0704441bd76d22f06380a4368826
shikalou <ldinaut@student.42.fr>
2025-03-15 00:30:59 +0100

    UPDATE + FIX SCRIPT WP

--- inception/srcs/requirements/mariadb/tools/script_db.sh
+++ inception/srcs/requirements/mariadb/tools/script_db.sh
@@ -1,21 +1,30 @@
 #!/bin/sh
+
+echo "Starting MySQL service"
 service mysql start
 
 sleep 3
 
+echo "Setting root password"
 mysqladmin -u root password ${SQL_ROOT_PASSWORD}
 
+echo "Creating db"
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
+echo "Creating user"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
+echo "granting privileges to user"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
 #mysql -e "ALTER USER 'root'@'%' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+echo "flushing privileges"
 mysql -e "FLUSH PRIVILEGES;"
+echo "shutting down mysql service"
 mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
 #mysqladmin --user=root --password=${SQL_ROOT_PASSWORD} shutdown
 
 sleep 3
 
+echo "start mysql server"
 exec mysqld_safe
--- inception/srcs/requirements/wordpress/tools/autoconf_wp.sh
+++ inception/srcs/requirements/wordpress/tools/autoconf_wp.sh
@@ -2,37 +2,49 @@
 
 sleep 10 
 
-#cd /var/www/wordpress
-
+wp --info
 #if ! wp core is-installed; then
-if [ ! -e /var/wwww/wordpress/wp-config.php ]; then
+echo "Checking if wp-config.php exists..."
+if [ ! -e /var/www/wordpress/wp-config.php ]; then
+	echo "wp-config.php does not exist. Creating..."
+	# wp config create \
+	# 			--dbname=$SQL_DATABASE \
+	# 			--dbuser=$SQL_USER \
+	# 			--dbpass=$SQL_PASSWORD \
+	# 			--dbhost=mariadb:3306 \
+	# 			--path='/var/www/wordpress'\
+	# 			--allow-root
+	sed -i "s/votre_nom_de_bdd/$SQL_DATABASE/g" /var/www/wordpress/wp-config-sample.php
+	sed -i "s/votre_utilisateur_de_bdd/$SQL_USER/g" /var/www/wordpress/wp-config-sample.php
+	sed -i "s/votre_mdp_de_bdd/$SQL_PASSWORD/g" /var/www/wordpress/wp-config-sample.php
+	sed -i "s/localhost/mariadb:3306/g" /var/www/wordpress/wp-config-sample.php
+	cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
+	# rm /var/www/wordpress/wp-config-sample.php
 
-wp config create	--dbname=$SQL_DATABASE \
-			--dbuser=$SQL_USER \
-			--dbpass=$SQL_PASSWORD \
-			--dbhost=mariadb:3306 \
-		       	--path='/var/www/wordpress'\
-			--allow-root
+	echo "Installing WordPress core..."
+	wp core install \
+				--url=$DOMAIN_NAME \
+				--title="inception" \
+				--admin_user=$ADMIN_USER \
+				--admin_password=$ADMIN_PASSWORD \
+				--admin_email=$ADMIN_EMAIL\
+				--path='/var/www/wordpress' \
+				--allow-root
 
-wp core install		--url=$DOMAIN_NAME \
-			--title="inception" \
-			--admin_user=$ADMIN_USER \
-			--admin_password=$ADMIN_PASSWORD \
-			--admin_email=$ADMIN_EMAIL\
-			--allow-root\
-		       	--path='/var/www/wordpress'
-
-
-wp user create		$USER1_LOGIN $USER1_MAIL \
-			--role=author \
-			--user_pass=$USER1_PASSWORD\
-			--allow-root\
-		       	--path='/var/www/wordpress'
-
+	echo "Creating user..."
+	wp user create		$USER1_LOGIN $USER1_MAIL \
+				--role=author \
+				--user_pass=$USER1_PASSWORD\
+				--allow-root\
+					--path='/var/www/wordpress'
+else
+	echo "wp-config.php already exists."
 fi
+
 if [ ! -d /run/php ]; then
 	mkdir -p /run/php;
 fi
 
+echo "Starting PHP-FPM..."
 # start the PHP FastCGI Process Manager (FPM) for PHP version 7.3 in the foreground
 /usr/sbin/php-fpm7.3 -F
