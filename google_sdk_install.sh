1998b426727b7f4180de12fd1c68ecdf831fd3c1
Luna DINAUT <ldinaut@f6r8s2.clusters.42paris.fr>
2026-04-10 16:51:54 +0200

    slkfjsldkfjs

--- google_sdk_install.sh
+++ google_sdk_install.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
 
-curl https://sdk.cloud.google.com > install.sh
+# Update and install dependencies
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https ca-certificates gnupg curl
 
-bash install.sh --disable-prompts
+# --- Install Terraform ---
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt-get update && sudo apt-get install terraform -y
+
+# --- Install GCloud CLI ---
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+sudo apt-get update && sudo apt-get install google-cloud-cli -y
+
+echo "Installations complete. Version check:"
+terraform -version
+gcloud --version
